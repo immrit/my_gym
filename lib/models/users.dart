14b01072cdc3551fbf9608b6a10f88e14a256fbf
mrit <ahmadesmaili.official@gmail.com>
2023-08-17 00:22:41 +0330

    latest 17 ago

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,9 +1,19 @@
+import 'package:hive/hive.dart';
+part 'users.g.dart';
+
+@HiveType(typeId: 0)
 class Users {
+  @HiveField(0)
   int id;
+  @HiveField(1)
   String name;
+  @HiveField(2)
   String nationalCode;
+  @HiveField(3)
   String fatherName;
+  @HiveField(4)
   String date;
+  @HiveField(5)
   bool gender;
 
   Users({
---
+++ lib/models/users.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'users.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class UsersAdapter extends TypeAdapter<Users> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Users read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Users(
+      id: fields[0] as int,
+      name: fields[1] as String,
+      nationalCode: fields[2] as String,
+      fatherName: fields[3] as String,
+      date: fields[4] as String,
+      gender: fields[5] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Users obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.nationalCode)
+      ..writeByte(3)
+      ..write(obj.fatherName)
+      ..writeByte(4)
+      ..write(obj.date)
+      ..writeByte(5)
+      ..write(obj.gender);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is UsersAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
